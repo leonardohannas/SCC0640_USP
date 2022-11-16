b455d158916f0620b8dfe9d6692129cf3abce481
LucasStapf <lucas.c.stapf@gmail.com>
2022-11-16 14:40:19 -0300

    Alterações gerais.

--- StickerBOOM/src/main/java/br/com/stickerboom/HelloApplication.java
+++ StickerBOOM/src/main/java/br/com/stickerboom/HelloApplication.java
@@ -6,9 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class HelloApplication extends Application {
     @Override
@@ -24,10 +22,16 @@ public class HelloApplication extends Application {
 
         try {
 
-            String user = "";
-            String password = "";
+            String user = "a11800559";
+            String password = "stickerboom";
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//orclgrad1.icmc.usp.br:1521/pdb_elaine.icmc.usp.br", user, password);
             System.out.println("Conectou!");
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM CARGO");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + "| " + resultSet.getString(2));
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
