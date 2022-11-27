0f679e73754ce73e86a12e4c5026997ca11b1f94
LeonardoHannas <68200228+LeonardoHannas@users.noreply.github.com>
2022-11-27 16:05:27 -0300

    Inserida a busca que retorna a linha da tabela AVALIACAO_VENDA de maior valor de VENDA

--- StickerBOOM/src/main/SQL/consultas.sql
+++ StickerBOOM/src/main/SQL/consultas.sql
@@ -7,6 +7,19 @@
 
 -- Busca pela avaliação da venda de maior valor --
 SELECT NEGOCIACAO, VALOR FROM VENDA
-    WHERE VALOR = (SELECT MAX(VALOR) FROM VENDA)
-    -- achar na aval_venda o colecionador 1 e data_hora
-    -- achar na avaliacao o colecionador 1 e data_hora
+    WHERE VALOR = (SELECT MAX(VALOR) FROM VENDA);
+--     achar na aval_venda o colecionador 1 e data_hora
+--     achar na avaliacao o colecionador 1 e data_hora
+
+-- Retorna a linha da tabela AVALIACAO_VENDA de maior valor de VENDA
+SELECT AVALIACAO_VENDA.COLECIONADOR_1, AVALIACAO_VENDA.DATA_HORA, AVALIACAO_VENDA.NEGOCIACAO, AVALIACAO_VENDA.COLECIONADOR_2 FROM AVALIACAO_VENDA JOIN VENDA
+ON AVALIACAO_VENDA.NEGOCIACAO = VENDA.NEGOCIACAO AND AVALIACAO_VENDA.COLECIONADOR_2 = VENDA.COLECIONADOR
+WHERE VENDA.VALOR = (SELECT MAX(VALOR) FROM VENDA);
+
+
+
+
+
+
+
+
