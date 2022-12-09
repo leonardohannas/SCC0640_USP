d390e1df02ff132f709ced177017de75c19ccce4
LucasStapf <lucas.c.stapf@gmail.com>
2022-12-09 08:04:09 -0300

    Adicionadas funcionalidades para álbuns do colecionador.

--- StickerBOOM/src/main/java/br/com/stickerboom/view/MainScreenCollectorController.java
+++ StickerBOOM/src/main/java/br/com/stickerboom/view/MainScreenCollectorController.java
@@ -9,6 +9,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.List;
@@ -19,6 +22,9 @@ public class MainScreenCollectorController implements Initializable {
     @FXML
     private ListView<Album> listViewAlbumsSystem;
     private List<Album> listS;
+
+    @FXML
+    private Text titleSearchPage;
 
     @FXML
     private TextField searchTitleField;
@@ -32,24 +38,70 @@ public class MainScreenCollectorController implements Initializable {
     @FXML
     private Label reputationCollectorLabel;
 
+    @FXML
+    private HBox availableAlbums;
+
+    @FXML
+    private HBox myAlbums;
+
+    private enum Window {
+
+        AVAILABLE_ALBUMS {
+            @Override
+            public String getTitle() {
+                return "Álbuns Disponíveis";
+            }
+        },
+        MY_ALBUMS {
+            @Override
+            public String getTitle() {
+                return "Meus Álbuns";
+            }
+        };
+
+        public abstract String getTitle();
+    }
+
+    private Window currentWindow = Window.AVAILABLE_ALBUMS;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         collectorNameLabel.setText(ScreenManager.getUser().getName());
-//        addressCollectorLabel.setText(ScreenManager.getUser().getAddress());
         reputationCollectorLabel.setText("Reputação: " + Float.toString(((Collector) ScreenManager.getUser()).getReputation()));
-//
+        titleSearchPage.setText(currentWindow.getTitle());
+
         listViewAlbumsSystem.setCellFactory(new AlbumCellFactory());
-//        listViewAlbumsCollector.setCellFactory(new VirtualAlbumCellFactory());
-//
-//        List<Album> listS = CommonQueries.getAllAlbumsExcept((Collector) ScreenManager.getUser());
+
+        availableAlbums.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getButton() == MouseButton.PRIMARY
+                    && currentWindow != Window.AVAILABLE_ALBUMS) {
+                currentWindow = Window.AVAILABLE_ALBUMS;
+                titleSearchPage.setText(currentWindow.getTitle());
+                listViewAlbumsSystem.getItems().clear();
+                listViewAlbumsSystem.setCellFactory(new AlbumCellFactory());
+                listS = Queries.getAllAlbumsExcept((Collector) ScreenManager.getUser());
+                for (Album album : listS)
+                    listViewAlbumsSystem.getItems().add(album);
+            }
+        });
+
+        myAlbums.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getButton() == MouseButton.PRIMARY
+                    && currentWindow != Window.MY_ALBUMS) {
+                currentWindow = Window.MY_ALBUMS;
+                titleSearchPage.setText(currentWindow.getTitle());
+                listViewAlbumsSystem.getItems().clear();
+                listViewAlbumsSystem.setCellFactory(new VirtualAlbumCellFactory());
+                listS = Queries.getAllAlbumsFrom((Collector) ScreenManager.getUser());
+                for (Album album : listS)
+                    listViewAlbumsSystem.getItems().add(album);
+            }
+        });
+
         listS = Queries.getAllAlbumsExcept((Collector) ScreenManager.getUser());
         for (Album album : listS)
             listViewAlbumsSystem.getItems().add(album);
-//
-//        List<Album> listC = CommonQueries.getAllAlbumsFrom((Collector) ScreenManager.getUser());
-//        for (Album album : listC)
-//            listViewAlbumsCollector.getItems().add(album);
-//
     }
 
     public void onActionSearch(ActionEvent actionEvent) {
